bf423c2cc5d0fca8b70e46bc202afe9cf573649a
imdokdokhe <tonykslee1@gmail.com>
2016-10-10 14:11:55 -0700

    branchtest1

---
+++ app/src/main/java/com/example/myfirstapp/branchtest1.java
@@ -0,0 +1,13 @@
+package com.example.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class branchtest1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_branchtest1);
+    }
+}
